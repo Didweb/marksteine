fb774b4712c3913265ffa74eeb72e86a175a90cc
Edu <info@did-web.com>
2018-01-03 22:11:07 +0100

    Basic test

--- src/AppBundle/Tests/Controller/CountryControllerTest.php
+++ src/AppBundle/Tests/Controller/CountryControllerTest.php
@@ -6,4 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CountryControllerTest extends WebTestCase
 {
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/country/list');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
 }
